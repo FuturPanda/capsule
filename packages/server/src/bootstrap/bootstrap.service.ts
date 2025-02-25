619bfe8e6ede7d77fffb5b0fd11b953010d2d366
Hoareau.Steevy <68106917+FuturPanda@users.noreply.github.com>
2025-02-25 16:02:31 +0100

    fix: add env for capsule cloud url

--- packages/server/src/bootstrap/bootstrap.service.ts
+++ packages/server/src/bootstrap/bootstrap.service.ts
@@ -16,8 +16,6 @@ export enum TestEnum {
   ONE = 'ONE',
 }
 
-export const CLOUD_CAPSULE_PROVIDER_URL = 'http://localhost:5173'; //'https://www.caspule.sh';
-
 @Injectable()
 export class BootstrapService
   implements OnApplicationBootstrap, OnApplicationShutdown
@@ -46,8 +44,10 @@ export class BootstrapService
       UserTypeEnum.OWNER,
     );
     const apiKey = this.apiKeysService.createApiKeyIfNotExists();
-    if (this.configService.get('IS_CLOUD_PROVIDED')) {
-      fetch(`${CLOUD_CAPSULE_PROVIDER_URL}/api/callback`, {
+    const isCloudProvided = this.configService.get('IS_CLOUD_PROVIDED');
+    const callbackUrl = this.configService.get('CLOUD_CAPSULE_CALLBACK_URL');
+    if (isCloudProvided && callbackUrl) {
+      fetch(callbackUrl, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
